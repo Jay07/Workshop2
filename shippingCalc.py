385e218429b2906d2cfd670328f4950b357079ae
jc338335 <leong.cheichau@dhs.sg>
2016-05-24 14:39:32 +0800

    added comments to shippingCalc.py

--- shippingCalc.py
+++ shippingCalc.py
@@ -1,14 +1,20 @@
-
+# input number of items
 itemNo=int(input('Number of items:'))
 
+# loop
 while itemNo<=0:
     print('Invalid number of items!')
     itemNo=int(input('Number of items:'))
 
+# input cost of item
 costPerItem=float(input('Shipping cost per item:'))
 
+# calculate total cost of items
 total=itemNo*costPerItem
 
+# discount
 if total>100:
     total-=(total*10/100)
+
+# print statement
 print('Total shipping cost:${:,.2f}'.format(total))
